f6527cdff98d97094d14ebe4a67af31d37a751a5
Dan Aloni <dan@kernelim.com>
2019-04-28 11:10:49 +0300

    Changes representative of linux-4.18.0-80.1.2.el8_0.tar.xz

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -25,6 +25,7 @@
 #include <linux/stop_machine.h>
 #include <linux/types.h>
 #include <linux/mm.h>
+#include <linux/cpu.h>
 #include <asm/cpu.h>
 #include <asm/cpufeature.h>
 #include <asm/cpu_ops.h>
@@ -887,7 +888,7 @@ static bool unmap_kernel_at_el0(const struct arm64_cpu_capabilities *entry,
 		MIDR_ALL_VERSIONS(MIDR_BRCM_VULCAN),
 		{ /* sentinel */ }
 	};
-	char const *str = "command line option";
+	char const *str = "kpti command line option";
 
 	/*
 	 * For reasons that aren't entirely clear, enabling KPTI on Cavium
@@ -896,6 +897,11 @@ static bool unmap_kernel_at_el0(const struct arm64_cpu_capabilities *entry,
 	 */
 	if (cpus_have_const_cap(ARM64_WORKAROUND_CAVIUM_27456)) {
 		str = "ARM64_WORKAROUND_CAVIUM_27456";
+		__kpti_forced = -1;
+	}
+
+	if (cpu_mitigations_off() && !__kpti_forced) {
+		str = "mitigations=off";
 		__kpti_forced = -1;
 	}
 
